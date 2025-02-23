ae9f7c6395643ded71143fa734c1cc185e491ca9
Elav Arivalagan <elavarasan.a@gmail.com>
2025-02-22 16:21:45 -0800

    parquet converter

--- simple.py
+++ simple.py
@@ -1,13 +1,14 @@
-# File name: serve_quickstart.py
 from starlette.requests import Request
 
 import ray
 from ray import serve
 
 @serve.deployment(num_replicas=2, ray_actor_options={"num_cpus": 0.2, "num_gpus": 0})
-class Translator:
+class RayApi:
     async def __call__(self, http_request: Request) -> str:
-        english_text: str = await http_request.json()
-        return f"Thank God It's working - {english_text}"
+        csv_loc: str = await http_request.json()
+        ds = ray.data.read_csv(csv_loc)
+        ds.write_parquet(csv_loc.replace(".csv", ".parquet"))
+        return f"The file {csv_loc} has been converted to parquet"
 
-app = Translator.bind()
+app = RayApi.bind()
